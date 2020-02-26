eb96b6a251821601f3a22b3f17df12cfe6fb54c8
sammiefrog <sammiefrog@live.com>
2020-02-26 15:56:24 -0600

    grabbed all the classes needed from the card in html

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var questionNum = document.querySelector(".question-number");
+var questionCont = document.querySelector(".question-content");
+var answerOne = document.querySelector(".answer-one");
+var answerTwo = document.querySelector(".answer-two");
+var answerThree = document.querySelector(".answer-three");
+var answerFour = document.querySelector(".answer-four");
+
